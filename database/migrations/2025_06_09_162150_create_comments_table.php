29b06c1b0f4fc5a4f424302da4ed6bbc6f066c07
naufal <naufal.nurhafizh.10.01@gmail.com>
2025-06-09 23:58:20 +0700

    [Migration] Create new table

--- database/migrations/2025_06_09_162150_create_comments_table.php
+++ database/migrations/2025_06_09_162150_create_comments_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->uuid('parent_id')->nullable();
+            $table->foreignUuid('post_id')->constrained()->onDelete('cascade');
+            $table->foreignUuid('user_id')->constrained()->onDelete('cascade');
+            $table->foreignUuid('reply_to_user_id')->nullable()->constrained('users')->onDelete('set null');
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 };
