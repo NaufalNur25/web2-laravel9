8712be520df50dcb461c08b76a46b7a8f465a1a2
Moch Rizaldi Ananda Kurnia <mochrizaldianandakurnia21@gmail.com>
2025-06-10 09:39:10 +0700

    create template frontend

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/home', function () {
+    return view('home');
+});
 
 Route::get('/submit', [App\Http\Controllers\SendSongController::class, 'formSubmit'])->name('form.submit');
 Route::post('/submit', [App\Http\Controllers\SendSongController::class, 'submit'])->name('song.submit');
