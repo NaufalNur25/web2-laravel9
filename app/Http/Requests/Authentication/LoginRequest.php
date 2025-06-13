9abbb8f5fe024f9647e040aeb618c3d37792ec08
naufal <naufal.nurhafizh.10.01@gmail.com>
2025-06-13 11:21:04 +0700

    [Added] Authentication process web service

---
+++ app/Http/Requests/Authentication/LoginRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Authentication;
+
+use Illuminate\Auth\Events\Lockout;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Str;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'username' => ['required', 'string', 'max:255'],
+            'password' => ['required', 'string', 'min:8'],
+        ];
+    }
+}
