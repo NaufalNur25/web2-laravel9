036ca2bfceac11cd39f846ebae36982049b2d9cc
naufal <naufal.nurhafizh.10.01@gmail.com>
2025-06-24 12:08:22 +0700

    [Adjust] Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
     <link rel="stylesheet" href="{{ asset('css/components/content-card.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/remixicon/fonts/remixicon.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <script src="https://open.spotify.com/embed/iframe-api/v1" async></script>
 
 </head>
@@ -36,12 +37,15 @@
     </nav>
     <main>
         <div class="posted">
-            @foreach ($posts as $index => $post)
-            <x-content-card
-                :id="'iframe-' . $index"
-                :username="$post->user->username ?? 'Unknown'"
-                :uri="$post->spotify_id ?? ''"
-                :description="$post->content ?? 'No Description'" />
+             @foreach ($posts as $post)
+                <x-content-card
+                    :id="$post->id"
+                    :userHavePost="auth()->id() === $post->user_id"
+                    :visibility="$post->visibility ?? false"
+                    :postedAt="$post->created_at->diffForHumans()"
+                    :username="$post->user->username ?? 'Unknown'"
+                    :uri="$post->spotify_id ?? ''"
+                    :description="$post->content ?? 'No Description'" />
             @endforeach
         </div>
     </main>
